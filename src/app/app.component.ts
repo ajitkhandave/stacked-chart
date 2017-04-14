77b27da97c7c08220db9095f4397d4a5755d17f5
ajitkhandave <ajitkhandave@gmail.com>
2017-04-14 17:51:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+   public barChartOptions:any = {
+    scales:{
+       xAxes: [{
+            stacked: true
+        }],
+      yAxes:[{
+        stacked:true
+      }]
+    }
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log();
+  }
+
+
 }
